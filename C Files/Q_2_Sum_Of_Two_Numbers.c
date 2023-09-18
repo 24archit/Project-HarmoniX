6696f305377925a4ec936573cf3a1b36a4739a3d
Archit Mishra <architmishra016@gmail.com>
2023-09-18 23:46:33 +0530

    Added a new file

---
+++ C Files/Q_2_Sum_Of_Two_Numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int num;
+    printf("Enter the First Number: \n");
+    scanf("%d", &num);
+    int b;
+    printf("Enter the Second Number: \n");
+    scanf("%d", &b);
+    printf("%d", num+b);
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # PROG
 Learning To Code <br>
 This Folder contains all basic problems for first learning in coding.
+<br>
+Learn Smarter, Learn Sharper !
