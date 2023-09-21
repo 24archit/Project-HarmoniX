5fe490c3444679744eba39b84624a6bb33bbb19b
Archit Mishra <architmishra016@gmail.com>
2023-09-21 18:02:30 +0530

    Added

--- C++ Files/Q_25_Maximum_Element_In_An_Array.cpp
+++ C++ Files/Q_25_Maximum_Element_In_An_Array.cpp
@@ -1,25 +1,27 @@
 #include<iostream>
 using namespace std;
-int main () {
+int max() {
     int n;
-    cout<<"Enter the nuhmber of elements: "<<endl;
+    cout<<"Enter the number of elements in the array: "<<endl;
     cin>>n;
     int arr[n];
-    cout<<"Enter the elements (Press Enter afer each number): "<<endl;
-    for(int i=0; i<n; i++) {
+    cout<<"Enter the elements of the array ( Press Enter afer each space)"<<endl;
+    for( int i =0; i<n; i++) {
         cin>>arr[i];
     }
-    for(int j =0; j<n; j++) {
-        int c=1;
-        for (int k=0; k<n; k++) {
-            if(arr[j] < arr[k]) {
-                 c=0;
-                 break;
+    for(int j=0; j<n; j++) {
+        int c =1;
+        for(int k=0; k<n; k++) {
+            if( arr[j] < arr[k]) {
+                c=0;
             }
         }
-        if(c==1) {
-        cout<<"Maximum valued element is: "<< arr[j];
+        if(c){
+        cout<<"The most valued element in the array is: "<<arr[j]<<endl;
+        }
     }
-    }
+}
+int main () {
+    max();
 return 0;
 }
