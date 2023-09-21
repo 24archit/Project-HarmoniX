ffba356b45da8ae2f9d6ad4840c016817f85cbd9
Archit Mishra <architmishra016@gmail.com>
2023-09-21 16:40:24 +0530

    Added

--- C Files/A_Rough_File.c
+++ C Files/A_Rough_File.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
-
 int main() {
-    int num = 9;
-    printf("Your answer is: %d.\nWelcome to the world of Mathematics !!\n", num);
-
+    int arr[7] ={2,3,4,10,12,4,2};
+    int check;
+    for( int i=0; i<7; i++) {
+        check =0;
+        for(int j =0; j<7; j++) {
+            if(arr[i]==arr[j] ) {
+                check =1;
+                printf("Hello World");
+                
+            }
+        }
+        if(check ==0) {
+            printf("%d ", arr[i]);
+        }
+    }
     return 0;
 }
--- C++ Files/Q_23_Find_Unique_Elements.cpp
+++ C++ Files/Q_23_Find_Unique_Elements.cpp
@@ -5,9 +5,9 @@ void unique (int arr[], int n) {
     for( int j=0; j<n; j++) {
         isduplicate = false;
         for(int k=0; k<n; k++) {
-            if(arr[j] ==arr[k] && j!=k) {
+            if(arr[j] ==arr[k] ) {
                 isduplicate = true;
-                break;
+               
             }  
         }
         if(!isduplicate) {
@@ -16,16 +16,16 @@ void unique (int arr[], int n) {
     }
 }
 int main() {
-    int a;
-    cin>>a;
-    for(int k=1; k<=a; k++){
+    //int a;
+    //cin>>a;
+    //for(int k=1; k<=a; k++){
     int n;
     cin>>n;
     int arr[n];
     for(int i=0; i<n; i++) {
         cin>>arr[i]; }
         unique(arr, n);
-    }
+    
 
     return 0;
 
