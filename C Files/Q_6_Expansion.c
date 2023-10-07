d5f8a8696f808f42a8b942a11bd0b29d34d494b0
Archit Mishra <architmishra016@gmail.com>
2023-10-07 19:18:13 +0530

    Added

---
+++ C Files/Q_6_Expansion.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    float a;
+    float b;
+    float sum =0;
+    printf("Enter the value of x: ");
+    scanf("%f", &a);
+    printf("Enter the accuracy for number of terms: ");
+    scanf("%f", &b);
+    for(int i=1; i<=a; )    
+    return 0;
+}
