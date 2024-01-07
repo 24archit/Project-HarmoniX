066be834b6f3d85ab389e04a11a3adba37c6d7af
Archit Mishra <architmishra016@gmail.com>
2024-01-07 20:29:21 +0530

    Added

--- C++ Files/A_Rough_File.cpp
+++ C++ Files/A_Rough_File.cpp
@@ -1,26 +1,8 @@
 #include <iostream>
 using namespace std;
-void primeFactorization(int n, int divisor = 2) {
-    if (n <= 1) {
-        return;
-    }
-    if (n % divisor == 0) {
-        cout << divisor << " "; 
-        while (n % divisor == 0) {
-            n =  n/divisor;
-        }
-    }
-    primeFactorization(n, divisor + 1);
-}
 int main() {
-    int num;
-    cout << "Enter a number to find its prime factorization: ";
-    cin >> num;
-    if (num <= 0) {
-        cout << "Please enter a positive integer.\n";
-        return 1; 
-    }
-    cout << "Prime Factorization of " << num << ": ";
-    primeFactorization(num);
+    int *arr ;
+    arr[0]=0;
+    cout<<arr[0];
     return 0; 
 }
