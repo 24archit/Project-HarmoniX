f0b3e2376953f0ff49853e152a01a00dda612868
Archit Mishra <architmishra016@gmail.com>
2024-05-19 18:49:50 +0530

    Added

--- C++ Files/Array/Q_55_OOPs.cpp
+++ C++ Files/Array/Q_55_OOPs.cpp
@@ -4,7 +4,7 @@ int main() {
     int number = 42;  // Replace 42 with any number you want to test
     int result = number | 1;
 
-    std::cout << "Result of " << number << " | 2: " << result << std::endl;
+    std::cout << "Result of " << number << " | 1: " << result << std::endl;
 
     return 0;
 }
