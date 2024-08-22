0f09af1fac63f8219c31dbb664e2d1aceaf3eca0
Archit Mishra <architmishra016@gmail.com>
2024-08-22 19:51:43 +0530

    Added for deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,  
-    rollupOptions: {
-      input: {
-        main: './index.html',  // Ensure Vite is using the right entry point
-      },
-    },
-  },
-})
+    outDir: 'dist' // Ensure this matches the directory in `vercel.json`
+  }
+});
