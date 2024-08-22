02ecd58b264c77abab8f5c673e2b680b764938e7
Archit Mishra <architmishra016@gmail.com>
2024-08-22 19:00:16 +0530

    Added for deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './',
   plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,  
+    rollupOptions: {
+      input: {
+        main: './index.html',  // Ensure Vite is using the right entry point
+      },
+    },
   },
 })
