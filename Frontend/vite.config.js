d6cb3f097c88283df998af09170f0aadeab7206f
Archit Mishra <architmishra016@gmail.com>
2024-08-22 18:29:03 +0530

    Added for deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from 'path';
-
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: resolve(__dirname, '../Backend/public/dist'), // output directory
-    emptyOutDir: true, 
+    emptyOutDir: true,  
   },
 })
