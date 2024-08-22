c032a520620267e156e6564fe4300be410bad7ad
Archit Mishra <architmishra016@gmail.com>
2024-08-22 21:16:07 +0530

    Added for deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Ensure this matches the directory in `vercel.json`
-  }
+    outDir: 'dist',
+  },
 });
