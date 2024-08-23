73c250594d939c02db4471776743d396f2d11742
Archit Mishra <architmishra016@gmail.com>
2024-08-23 22:19:26 +0530

    Added for deployment

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,16 @@
-import "./assets/styles/App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import Player from "./components/Player";
-import NavBar from "./components/NavBar";
-import LoginDialog from "./components/LoginDialog";
-import HomePage from "./pages/HomePage";
-import ArtistPage from "./pages/ArtistPage";
-import { getExpiryStatus } from "./apis/apiFunctions";
-import React, { useEffect, useState } from "react";
-import SearchPage from "./pages/SearchPage";
+import './assets/styles/App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+import NavBar from './components/NavBar';
+import LoginDialog from './components/LoginDialog';
+import HomePage from './pages/HomePage';
+import ArtistPage from './pages/ArtistPage';
+import { getExpiryStatus } from './apis/apiFunctions';
+import React, { useEffect, useState } from 'react';
+import SearchPage from './pages/SearchPage';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Cookie from "./components/Cookie";
 
 function App() {
   const [expiryCode, setExpiryCode] = useState(0);
@@ -24,7 +23,7 @@ function App() {
         console.log(expiryStatus);
         setExpiryCode(expiryStatus);
       } catch (error) {
-        console.error("Error fetching expiry status:", error);
+        console.error('Error fetching expiry status:', error);
       }
     };
 
@@ -32,14 +31,13 @@ function App() {
   }, []);
 
   // Conditionally render based on expiryCode
-  if (expiryCode === 0 || expiryCode === 1) {
+  if (expiryCode === 0) {
     return (
       <Router>
         <Routes>
           <Route path="/login" element={<LoginDialog />} />
-          <Route path="/" element={<LoginDialog />} />
-          {/* <Route path="/setting" element={<Cookie />} /> */}
-          <Route path="*" element={<LoginDialog />} />
+          <Route path="/" element={<LoginDialog/>}/>
+          <Route path="*" element={<LoginDialog/>}/>
         </Routes>
       </Router>
     );
@@ -51,46 +49,14 @@ function App() {
           <Sidebar />
           <div className="content">
             <Routes>
-              <Route
-                exact
-                path="/"
-                element={
-                  <main>
-                    <HomePage setNewUrl={setUrl} />
-                  </main>
-                }
-              />
-              <Route
-                exact
-                path="/user/home"
-                element={
-                  <main>
-                    <HomePage setNewUrl={setUrl} />
-                  </main>
-                }
-              />
-              <Route
-                exact
-                path="/user/search"
-                element={
-                  <main>
-                    <SearchPage setNewUrl={setUrl} />
-                  </main>
-                }
-              />
-              <Route
-                exact
-                path="/user/artist/:id"
-                element={
-                  <main>
-                    <ArtistPage setNewUrl={setUrl} />
-                  </main>
-                }
-              />
-              <Route exact path="/user/playlist/:id" element={<main></main>} />
+              <Route exact path="/" element={<main><HomePage setNewUrl={setUrl} /></main>} />
+              <Route exact path="/user/home" element={<main><HomePage setNewUrl={setUrl} /></main>} />
+              <Route exact path="/user/search" element={<main><SearchPage setNewUrl={setUrl} /></main>} />
+              <Route exact path="/user/artist/:id" element={<main><ArtistPage setNewUrl={setUrl} /></main>} />
+              <Route exact path="/user/playlist/:id" element={<main></main>}/>
             </Routes>
           </div>
-          <Player url={url} setNewUrl={setUrl} />
+          <Player url={url} setNewUrl={setUrl}/>
         </div>
       </Router>
     );
