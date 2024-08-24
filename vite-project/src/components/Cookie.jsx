600f0801613be63b56992c0c8abf32bb18945d1c
Archit Mishra <architmishra016@gmail.com>
2024-08-24 14:05:13 +0530

    Added for deployment

--- vite-project/src/components/Cookie.jsx
+++ vite-project/src/components/Cookie.jsx
@@ -4,20 +4,13 @@ export default function Cookie() {
     const setCookieData = async () => {
       // Original URLs
       const firstUrl = window.location.href;
-      const secondUrl = "https://harmonix-stream.vercel.app";
+      const secondUrl = "https://harmonix-stream.vercel.app/callback";
 
       // Create a URL object for the first URL to extract query parameters
       const urlObj1 = new URL(firstUrl);
       const queryParams = urlObj1.search; // This gives you "?param1=value1&param2=value2"
 
-      // Create a URL object for the second URL
-      const urlObj2 = new URL(secondUrl);
-
-      // Append the query parameters to the second URL
-      urlObj2.search = queryParams;
-
-      // Convert the updated URL object back to a string
-      const updatedUrl = urlObj2.toString();
+      const updatedUrl = secondUrl + queryParams;
 
       const response = await fetch(updatedUrl);
       if (!response.ok) {
