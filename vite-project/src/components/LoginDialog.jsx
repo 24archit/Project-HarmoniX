ad704463cb8c7f00d212dba6d50e7856f12498ab
Archit Mishra <architmishra016@gmail.com>
2024-08-25 15:09:10 +0530

    Added for deployment

--- vite-project/src/components/LoginDialog.jsx
+++ vite-project/src/components/LoginDialog.jsx
@@ -2,6 +2,15 @@ import { useEffect, useState } from "react";
 import "../assets/styles/LoginDialog.css";
 import { useParams } from 'react-router-dom';
 
+function generateRandomString(length) {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 export default function LoginDialog() {
     const[error, setError] = useState("");
     useEffect(() => {
@@ -12,18 +21,7 @@ export default function LoginDialog() {
     
         fetchError();
       }, []);
-    
-
-    function generateRandomString(length) {
-        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        let result = "";
-        const charactersLength = characters.length;
-        for (let i = 0; i < length; i++) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
-    }
-
+      
     const handleClick = () => { // Fixed typo in function name
         const originalState = generateRandomString(16);
         localStorage.setItem('state', `${originalState}`);
