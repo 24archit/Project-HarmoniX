649ef0ea29c3bd044b23cd70001365e2c8e190f9
Archit Mishra <architmishra016@gmail.com>
2024-10-11 21:55:53 +0530

    Removed Vulnerabilities

--- vite-project/src/components/TrackImg.jsx
+++ vite-project/src/components/TrackImg.jsx
@@ -1,20 +1,13 @@
-import React from 'react';
+import React from "react";
 import TrackLogo from "../assets/media/Animated-Track-Logo.gif";
 import "../assets/styles/TrackImg.css";
 
 export function TrackImg() {
-   
-
-    return (
-        <>
-            <div
-                id="track-img"
-                style={{
-                    boxShadow: `0 4px 8px 0 ${data}, 0 6px 20px 0 ${data}`,
-                }}
-            >
-                <img src={TrackLogo} alt="Track Logo" />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div id="track-img">
+        <img src={TrackLogo} alt="Track Logo" />
+      </div>
+    </>
+  );
 }
