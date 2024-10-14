7c2ef3c7204d0b7187d2c4e40e6eaa98902517c9
Archit Mishra <architmishra016@gmail.com>
2024-10-14 16:24:15 +0530

    Fixed a authentication bug

--- vite-project/src/apis/apiFunctions.js
+++ vite-project/src/apis/apiFunctions.js
@@ -3,80 +3,80 @@ const protocol = "https";
 const host = "harmonix-stream.vercel.app";
 
 
-function checkExpiry() {
-  const cookie = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("userdetails="));
+// function checkExpiry() {
+//   const cookie = document.cookie
+//     .split("; ")
+//     .find((row) => row.startsWith("userdetails="));
 
-  if (!cookie) {
-    return 0;
-  }
+//   if (!cookie) {
+//     return 0;
+//   }
 
-  // Extract the cookie value and decode it
-  const cookieValue = cookie.split("=")[1];
-  const decodedValue = decodeURIComponent(cookieValue);
+//   // Extract the cookie value and decode it
+//   const cookieValue = cookie.split("=")[1];
+//   const decodedValue = decodeURIComponent(cookieValue);
 
-  // Parse the JSON string into an object
-  let userdetails;
-  try {
-    userdetails = JSON.parse(decodedValue);
-  } catch (e) {
-    console.log("Error parsing cookie");
-    return 0;
-  }
+//   // Parse the JSON string into an object
+//   let userdetails;
+//   try {
+//     userdetails = JSON.parse(decodedValue);
+//   } catch (e) {
+//     console.log("Error parsing cookie");
+//     return 0;
+//   }
 
-  // Check the conditions
-  if (!userdetails) {
-    return 0;
-  } else if (userdetails.expiry < Date.now()) {
-    return 1;
-  } else {
-    return 2;
-  }
-}
+//   // Check the conditions
+//   if (!userdetails) {
+//     return 0;
+//   } else if (userdetails.expiry < Date.now()) {
+//     return 1;
+//   } else {
+//     return 2;
+//   }
+// }
 async function fetchData(url, method, errorMessage) {
-  const cookie = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("userdetails="));
-  const cookieValue = cookie.split("=")[1];
-  const decodedValue = decodeURIComponent(cookieValue);
-  let userdetails;
-  userdetails = JSON.parse(decodedValue);
+  // const cookie = document.cookie
+  //   .split("; ")
+  //   .find((row) => row.startsWith("userdetails="));
+  // const cookieValue = cookie.split("=")[1];
+  // const decodedValue = decodeURIComponent(cookieValue);
+  // let userdetails;
+  // userdetails = JSON.parse(decodedValue);
 
-  const expiryStatus = checkExpiry();
-  if (expiryStatus === 1) {
+  // const expiryStatus = checkExpiry();
+  // if (expiryStatus === 1) {
 
-    const userdetailsNew= {
-      userId: userdetails.userId,
-      expiry: Date.now() + 3000000,
-    }
-    const userdetailsStr = JSON.stringify(userdetailsNew);
-    document.cookie = `userdetails=${encodeURIComponent(
-      userdetailsStr
-    )}; max-age=${15 * 24 * 60 * 60}; HttpOnly; secure;`;
+  //   const userdetailsNew= {
+  //     userId: userdetails.userId,
+  //     expiry: Date.now() + 3000000,
+  //   }
+  //   const userdetailsStr = JSON.stringify(userdetailsNew);
+  //   document.cookie = `userdetails=${encodeURIComponent(
+  //     userdetailsStr
+  //   )}; max-age=${15 * 24 * 60 * 60}; HttpOnly; secure;`;
 
-    try {
-      const response = await fetch(url, {
-        method: method,
-        headers: {
-          "local-api-access-token":
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
-          "expiry-code": "1",
-          "user-id": `${userdetails.userId}`,
-        },
-      });
-      if (!response.ok) {
-        throw new Error(errorMessage);
-      }
-      const data = await response.json();
-      return JSON.parse(data);
-    } catch (error) {
-      document.cookie = "userdetails=; max-age=0; secure;";
-      window.location.href=`https://harmonix-play.vercel.app/login?error=${error}`;
-      throw new Error(error);
-    }
-  }
-  if (expiryStatus === 2) {
+  //   try {
+  //     const response = await fetch(url, {
+  //       method: method,
+  //       headers: {
+  //         "local-api-access-token":
+  //           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+  //         "expiry-code": "1",
+  //         "user-id": `${userdetails.userId}`,
+  //       },
+  //     });
+  //     if (!response.ok) {
+  //       throw new Error(errorMessage);
+  //     }
+  //     const data = await response.json();
+  //     return JSON.parse(data);
+  //   } catch (error) {
+  //     document.cookie = "userdetails=; max-age=0; secure;";
+  //     window.location.href=`https://harmonix-play.vercel.app/login?error=${error}`;
+  //     throw new Error(error);
+  //   }
+  // }
+  // if (expiryStatus === 2) {
     try {
       const response = await fetch(url, {
         method: method,
@@ -97,11 +97,11 @@ async function fetchData(url, method, errorMessage) {
       window.location.href=`https://harmonix-play.vercel.app/login?error=${error}`;
       throw new Error(error);
     }
-  } else {
-    document.cookie = "userdetails=; max-age=0; secure;";
-    window.location.href = "https://harmonix-play.vercel.app/login";
-    return;
-  }
+  // } else {
+  //   document.cookie = "userdetails=; max-age=0; secure;";
+  //   window.location.href = "https://harmonix-play.vercel.app/login";
+  //   return;
+  // }
 }
 
 export async function getTopTracksIndia() {
