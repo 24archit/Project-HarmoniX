f548370a8da06c0f70a26aab037a1d883273c9f7
Archit Mishra <architmishra016@gmail.com>
2024-10-20 01:39:50 +0530

    Taken step for responsiveness

--- vite-project/src/components/SectionName.jsx
+++ vite-project/src/components/SectionName.jsx
@@ -1,39 +1,35 @@
 import "../assets/styles/SectionName.css";
 import { Skeleton } from "@mui/material";
+
 export function SectionName({
-  iconClass="",
-  iconId="",
-  name="",
-  button=true
+  iconClass = "",
+  iconId = "",
+  name = "",
+  button = true
 }) {
   return (
     <div className="section-name">
       <li>
         <i className={iconClass} id={iconId}></i>
-         {name}
+        {name}
       </li>
-      {button == true ? <button className="more-btn-home">See All</button> : <></>}
+      {button ? <button className="more-btn-home">See All</button> : null}
     </div>
   );
 }
+
 export function SectionNameLoad() {
   return (
-    
-     
-      <Skeleton
-                variant="Rectangular"
-                width={700}
-                height={30}
-                sx={{
-                    marginLeft: '1rem',
-                    marginRight: '1rem',
-                    bgcolor: 'rgba(71, 164, 211, 0.261)',
-                    borderRadius: '1rem'
-                    
-
-                }}
-            />
-      
-    
+    <Skeleton
+      variant="rectangular"
+      width={700}
+      height={30}
+      sx={{
+        marginLeft: '1rem',
+        marginRight: '1rem',
+        bgcolor: 'rgba(71, 164, 211, 0.261)',
+        borderRadius: '1rem'
+      }}
+    />
   );
 }
